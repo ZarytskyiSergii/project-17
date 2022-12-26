697d7d42902328a10bfcb302ed3617ddccce742b
ZarytskyiSergii <114443256+ZarytskyiSergii@users.noreply.github.com>
2022-12-26 17:14:14 +0000

    Deploying to gh-pages from @ ZarytskyiSergii/project-17@2c551261a6450331e7ed34f6c8e25029b36f312b 🚀

--- index.a0131dfb.js
+++
@@ -1,2 +0,0 @@
-var swiper_customers=new Swiper(".customers__slider",{pagination:{el:".swiper-pagination",clickable:!0},dots:!1,infinite:!0,speed:3e3,slidesToShow:1,adaptiveHeight:!0,autoplay:!0,autoplaySpeed:2e3}),swiper_gallery=new Swiper(".gallery__slider",{dots:!1,infinite:!0,speed:1e3,slidesToShow:1,adaptiveHeight:!0,autoplay:!0,autoplaySpeed:2e3});
-//# sourceMappingURL=index.a0131dfb.js.map
