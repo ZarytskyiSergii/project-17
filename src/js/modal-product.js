d0c43a0910839054a58e8793e4af33f2f4cadedb
Sergii <sonik2552@gmail.com>
2022-12-28 00:39:25 +0100

    commit

--- src/js/modal-product.js
+++ src/js/modal-product.js
@@ -1,12 +1,18 @@
 (() => {
   const refs = {
-    openModalPro: document.querySelector('[data-modal-open-product]'),
+    openModalPro1: document.querySelector('[data-modal-open-product1]'),
+    openModalPro2: document.querySelector('[data-modal-open-product2]'),
+    openModalPro3: document.querySelector('[data-modal-open-product3]'),
+
     closeModalPro: document.querySelector('[data-modal-close-product]'),
     modal: document.querySelector('[data-modal-product]'),
     body: document.querySelector('[data-body-modal]'),
   };
 
-  refs.openModalPro.addEventListener('click', toggleModal);
+  refs.openModalPro1.addEventListener('click', toggleModal);
+  refs.openModalPro2.addEventListener('click', toggleModal);
+  refs.openModalPro3.addEventListener('click', toggleModal);
+
   refs.closeModalPro.addEventListener('click', toggleModal);
 
   function toggleModal() {
