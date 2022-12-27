9a89f867f841146313a800c3ed362ed70fa09414
mel1996 <78021970+mel1996@users.noreply.github.com>
2022-12-27 23:59:28 +0200

    add modal-video

---
+++ src/js/modal-video.js
@@ -0,0 +1,15 @@
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-open-video]"),
+      closeModalBtn: document.querySelector("[data-modal-close-video]"),
+      modal: document.querySelector("[data-modal-video]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+    }
+  })();
+  
