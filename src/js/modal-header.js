7d5c190455039894fde28b442e8b607893b02208
Angela Ponomarenko <angelkapv81@gmail.com>
2022-12-28 02:49:47 +0200

    fix

--- src/js/modal-header.js
+++ src/js/modal-header.js
@@ -1,18 +1,16 @@
 (() => {
   const refs = {
-    openModalBtn1: document.querySelector("[data-modal-open-header1]"),
-    openModalBtn: document.querySelector("[data-modal-open-header]"),
-    closeModalBtn: document.querySelector("[data-modal-close-header]"),
-    modal: document.querySelector("[data-modal-header]"),
-    body: document.querySelector("[data-body-modal]")
+    openModalBtn1: document.querySelector('[data-modal-open-header1]'),
+    openModalBtn: document.querySelector('[data-modal-open-header]'),
+    closeModalBtn: document.querySelector('[data-modal-close-header]'),
+    modal: document.querySelector('[data-modal-header]'),
   };
-  
-    refs.openModalBtn1.addEventListener("click", toggleModal);
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-      refs.body.classList.toggle("overflow");
-    }
-  })();
+
+  refs.openModalBtn1.addEventListener('click', toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
