e08c55fb5159f4d0cf78a87473de1af8d6e9325f
ZarytskyiSergii <114443256+ZarytskyiSergii@users.noreply.github.com>
2022-12-29 14:16:13 +0000

    Deploying to gh-pages from @ ZarytskyiSergii/project-17@da765b11f921cd2776ff277b8ff1d4d415ddd40f 🚀

--- index.0a813e62.js
+++
@@ -1,2 +0,0 @@
-$(".gallery__slider").slick({dots:!1,infinite:!0,speed:1e3,slidesToShow:1,adaptiveHeight:!0,autoplay:!0,autoplaySpeed:2e3}),$(".customers__slider").slick({dots:!0,infinite:!0,speed:300,slidesToShow:1,adaptiveHeight:!0});
-//# sourceMappingURL=index.0a813e62.js.map
