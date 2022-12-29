08d3ddec710fbdf96f958c7364f6ffe59d286b6c
Angela Ponomarenko <angelkapv81@gmail.com>
2022-12-29 16:36:54 +0200

    fix 16:36

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './js/swiper';
-
+import './js/modal-header';
+import './js/mobile-menu';
+import './js/modal-product';
+import './js/modal-franchize';
+import './js/modal-map';
+import './js/modal-video';
 
---
+++ src/js/modal-video.js
@@ -0,0 +1,17 @@
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-open-video]"),
+      closeModalBtn: document.querySelector("[data-modal-close-video]"),
+      modal: document.querySelector("[data-modal-video]"),
+       body: document.querySelector("[data-body-modal]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+      refs.body.classList.toggle("overflow");
+    }
+  })();
+  
